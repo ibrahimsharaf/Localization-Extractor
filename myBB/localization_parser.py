a909bf62031c56ee54b1c7f8e557f405c3c99e72
Ibrahim Sharaf <ibrahimsharafelden@gmail.com>
2017-06-17 23:37:37 +0200

    remove html tags, automate parsing from a directory

--- myBB/localization_parser.py
+++ myBB/localization_parser.py
@@ -3,6 +3,13 @@ import codecs
 import itertools
 import os
 import json
+import pathlib
+
+
+def clean_html(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
 
 
 def parse_one_file(filename):
@@ -17,7 +24,10 @@ def parse_one_file(filename):
 
     values = []
     for i in lines:
-        quoted = re.findall(r"['\"](.*?)['\"]", i)
+        cleaned_text = clean_html(i)
+        quoted_signle = re.findall(r"[\'](.*?)[\']", cleaned_text)
+        quoted_double = re.findall(r"[\"](.*?)[\"]", cleaned_text)
+        quoted = quoted_signle + quoted_double
         if quoted and len(quoted) == 2:
             values = values + quoted
 
@@ -28,21 +38,23 @@ def parse_one_file(filename):
 def get_all_files(directoryname):
     """
     Gets all configurations files inside a directory, parses them using parse_one_file function
-    :param directoryname: an arbitrary language file
-    :return: dict of all localization values
+    :param directoryname: directory
+    :return: dumps localization dictionaries in JSON format
     """
-    files_name = []
-    localization_values = {}
-    for filename in os.listdir(directoryname):
-        localization_values.update(parse_one_file(directoryname+'/'+filename))
-    return localization_values
+    for path, subdirs, files in os.walk(directoryname):
+        localization_values = {}
+        language = os.path.basename(path)
+        for name in files:
+            file_name = pathlib.PurePath(path, name)
+            localization_values.update(parse_one_file(file_name))
+        print(len(localization_values))
+        with open(language+'.json', 'w') as f:
+            json.dump(localization_values, f)
 
 
 def main():
-    path = '/home/ibrahimsharaf/Desktop/MyBB/english'
-    d = (get_all_files(path))
-    with open('english.json', 'w') as f:
-        json.dump(d, f)
+    path = '/home/ibrahimsharaf/Desktop/MyBB'
+    get_all_files(path)
 
 
 if __name__ == "__main__":
