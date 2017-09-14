c19615c4b270c15b9285a76c422bbcf482f73491
Ibrahim Sharaf <ibrahimsharafelden@gmail.com>
2017-09-14 14:45:58 +0200

    add merge_two_directories

--- merge_json.py
+++ merge_json.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 import codecs
+import os
+
 
 def merge(filename1, filename2, language):
     dict1 = json.load(codecs.open(filename1, 'r', encoding='utf-16'))
@@ -9,5 +11,20 @@ def merge(filename1, filename2, language):
     with open(language+'.json', 'w', encoding='utf-16') as f:
         json.dump(out, f, ensure_ascii=False)
 
-merge('/home/ibrahimsharaf/Desktop/NewForums/phpBB/singles/portuguese.json'
-      , '/home/ibrahimsharaf/Desktop/NewForums/phpBB/portuguese.json', 'portuguese')
+
+def get_files(directory1, directory2):
+    def get_files_from_directory(dir_name):
+        child_files = []
+        for path, subdirs, files in os.walk(dir_name):
+            language = os.path.basename(path)
+            for name in files:
+                child_files.append(path+'/'+name)
+        return child_files
+    files1 = get_files_from_directory(directory1)
+    files2 = get_files_from_directory(directory2)
+    for file1 in files1:
+        for file2 in files2:
+            if file1.split('/')[-1] == file2.split('/')[-1]:
+                merge(file1, file2, file1.split('/')[-1].split('.')[0])
+
+get_files('/home/ibrahimsharaf/Desktop/NewForums/xen+phpbb+vb', '/home/ibrahimsharaf/Desktop/NewForums/myBB')
