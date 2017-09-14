4aae5c06ff4a7acc90d2b23f9d2669ded5e64d5f
Ibrahim Sharaf <ibrahimsharafelden@gmail.com>
2017-09-14 21:03:08 +0200

    collect localization keys and values

--- hits_main.py
+++ hits_main.py
@@ -1,45 +1,55 @@
 import json
+import codecs
+import glob
 import itertools
 from collections import Counter
 
 
 def parse_singles(filename, language):
+    # parse single localization settings
     data = [line.strip() for line in open(filename, 'r')]
     d = dict(itertools.zip_longest(*[iter(data)] * 2, fillvalue=""))
     with open(language+'.json', 'w', encoding='utf-16') as file:
         json.dump(d, file, ensure_ascii=False)
 
 
-def hits_score(hit_path, local_path, language):
-    hits_lines = open(hit_path+language+'.txt').read().split("\n")
-    local_data = open(local_path+language+'.json').read()
-    d = json.loads(local_data)
+def get_all_localization_data(path):
+    # collect all keys and values from localization json files
+    keys = []
+    values = []
+    files = glob.glob(path)
+    for file in files:
+        loc_dict = json.load(codecs.open(file, 'r', encoding='utf-16'))
+        keys += list(loc_dict.keys())
+        values += list(loc_dict.values())
+    keys =[key.strip().lower() for key in keys]
+    values = [value.strip().lower() for value in values]
+    keys_file = open('keys.txt', 'w', encoding='utf-16')
+    for key in keys:
+        keys_file.write("%s\n" % key)
+    vals_file = open('values.txt', 'w', encoding='utf-16')
+    for val in values:
+        vals_file.write("%s\n" % val)
+    return
 
-    local_lines = []
-    for key, value in d.items():
-        local_lines.append(value)
 
-    if language is not 'English':
-        english_path = open('/home/ibrahimsharaf/Desktop/Localizations/English.json').read()
-        english_dict = json.loads(english_path)
-        for key, value in english_dict.items():
-            local_lines.append(value)
+def hits_score(hits_path, locals_path):
+    hits_lines = open(hits_path, encoding='utf-8').read().split("\n")
+    local_lines = open(locals_path, encoding='utf-16').read().split("\n")
 
     intersection = list((Counter(hits_lines) & Counter(local_lines)).elements())
     bp_set = set(intersection)
     hits_set = set(hits_lines)
     without_bp = hits_set-bp_set
-    with open(language+'_hits.txt', 'w') as f:
-        [f.write(i +'\n') for i in without_bp]
+    with open('results.txt', 'w', encoding='utf-16') as f:
+        [f.write(i + '\n') for i in bp_set]
     score = len(bp_set)/len(hits_set) * 100
     return str(round(score,2))
 
 
 def main():
-    hits_path='/home/ibrahimsharaf/Desktop/hittext_sample/'
-    localization_path='/home/ibrahimsharaf/Desktop/Localizations/'
-    print(hits_score(hits_path, localization_path, 'English'))
-    #parse_singles('/home/ibrahimsharaf/Desktop/NewForums/phpBB/Spanish_singles.txt', 'spanish')
+    print(hits_score('/home/ibrahimsharaf/Desktop/boilers.txt', '/home/ibrahimsharaf/Desktop/keys.txt'))
+    # get_all_localization_data('/home/ibrahimsharaf/Desktop/NewForums/all/*.json')
 
 if __name__ == "__main__":
     main()
